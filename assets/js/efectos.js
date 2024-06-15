d59f22afad71f4394e0885d934d226adeee84655
Pablo <56845322+AlmendroHub@users.noreply.github.com>
2024-06-15 15:56:59 +0200

    Update efectos.js

--- assets/js/efectos.js
+++ assets/js/efectos.js
@@ -1,3 +1,20 @@
-function test(){
-  console.log("JS Funcionando!!")
-}
+window.onload = function(){ 
+
+    console.log("JS Funcionando!!");
+
+    // <----- Menú Hmaburguesa Responsive ----->
+    const navbar = document.querySelector('#navbar');
+    const abrir = document.querySelector('#menu-desplegable');
+    const cerrar = document.querySelector('#cerrar-menu');
+
+    abrir.addEventListener("click", () => {
+        navbar.classList.add("visible")
+    })
+
+    cerrar.addEventListener("click", () => {
+        navbar.classList.remove("visible")
+    })
+
+    navbar.addEventListener("click", () => {
+        navbar.classList.remove("visible")
+    })
